c892349771f27b88e467c34439c814e2841a0bbd
RossMCDev <rossmdev@gmail.com>
2016-12-06 01:54:13 +0000

    Initial commit

---
+++ SeQuiz/Controllers/HomeController.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SeQuiz.Models;
+using System.Data.Entity;
+using System.Net;
+using System.Web.Mvc.Ajax;
+
+namespace SeQuiz.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private QuizDBEntities db = new QuizDBEntities();
+        List<Task> quiztasks = new List<Task>();
+        List<int> questionstoload = new List<int>();
+        Random random = new Random();
+
+        public ActionResult Index()
+        {
+            Quiz model = new Quiz();
+
+            if (model.CurrentTask == null)
+            {
+            ImageSet demoSet = new ImageSet();
+            demoSet.ImageName = "number";
+            demoSet.ImageOrder = "3421";
+            Task demo = new Task();
+            demo.Instruction = "Click and drag a picture on top of another to swap thier places";
+            model.CurrentTask = demo;
+            model.CurrentImageset = demoSet;
+            model.Score = 0;
+            }
+
+            Session["quiz"] = model;
+
+            return View(model);
+        }
+
+      
+                [HttpGet]
+        public ActionResult StartQuiz()
+        {
+            Quiz model = new Quiz();
+
+
+            
+            quiztasks.Clear();
+            //collection of model
+            var tasks = from s in db.Tasks
+                        select s;
+            int rannum;
+            int totalImageCount = 0;
+
+
+            while (totalImageCount < 4)
+            {
+                rannum = random.Next(1, tasks.Count() + 1);
+                if (!questionstoload.Contains(rannum))
+                {
+                    quiztasks.Add(tasks.First(x => x.Task_ID == rannum));
+                    questionstoload.Add(rannum);
+
+                    foreach (ImageSet imgs in quiztasks.Last().ImageSets)
+                    {
+                        totalImageCount++;
+                    }
+                }
+
+            }
+
+            model.Tasks = quiztasks;
+            model.CurrentTask = model.Tasks.First();
+            rannum = random.Next(0, model.CurrentTask.ImageSets.Count());
+            model.CurrentImageset = model.Tasks.First().ImageSets.ElementAt(rannum);
+            model.Finish = "Next"; 
+
+
+            Session["quiz"] = model;
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("QuizTasks", model);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [HttpGet]
+        public ActionResult NextQuestion(bool match,int minute, int secound)
+        {
+            Quiz model = new Quiz();
+
+            if (Session["quiz"] != null)
+            {
+                model = (Quiz)Session["quiz"];
+            }
+
+            if (match)
+            {
+                model.Score = model.Score + model.CurrentTask.Points;
+
+            }
+
+                model.ImageCount++;
+                if (model.ImageCount > model.CurrentTask.ImageSets.Count() - 1)
+                {
+                    model.ImageCount = 0;
+                    model.TaskCount++;
+          
+                }
+            model.RemainingMinutes = minute;
+            model.RemainingSecounds = secound;
+
+            if (model.TaskCount < model.Tasks.Count())
+            {
+                model.CurrentTask = model.Tasks.ElementAt(model.TaskCount);
+                model.CurrentImageset = model.Tasks.ElementAt(model.TaskCount).ImageSets.ElementAt(model.ImageCount);
+                if (model.TaskCount == model.Tasks.Count() - 1 && model.ImageCount == model.CurrentTask.ImageSets.Count() - 1)
+                { model.Finish = "Finish"; }
+
+                Session["quiz"] = model;
+
+                        if (Request.IsAjaxRequest())
+            {
+                return PartialView("QuizTasks", model);
+
+            }
+            else
+            {
+                return null;
+            }
+            
+            }
+            else
+            {
+                Session["quiz"] = model;
+
+                return RedirectToAction("ScoresOnBoard");
+            }
+
+
+        }
+
+
+        public ActionResult ScoresOnBoard()
+        {
+            Quiz model = new Quiz();
+            List<ScoreBoard> scoreboard = new List<ScoreBoard>();
+            if (Session["quiz"] != null)
+            {
+                model = (Quiz)Session["quiz"];
+            }
+            var scores = from s in db.ScoreBoards
+                         orderby s.Score descending
+                         select s;
+            scoreboard = scores.Take(10).ToList();
+            model.LeaderBoard = scoreboard;
+            foreach (ScoreBoard score in scoreboard)
+            {
+                if (score.Score < model.Score || scoreboard.Count < 10)
+                {
+                    model.NewHigh = true;
+                }
+            }
+            ScoreBoard scoreentry = new ScoreBoard();
+            double points = model.Score + (model.Score*((model.RemainingSecounds * 0.024) + model.RemainingMinutes * 1.5));
+            scoreentry.Score = Convert.ToInt32(points);
+            model.ScoreEntry = scoreentry;
+            return View(model);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ScoresOnBoard(Quiz scoreboard)
+
+        {
+            List<ScoreBoard> leaderboard = new List<ScoreBoard>();
+
+      
+
+
+            if (ModelState.IsValid)
+            {
+                db.ScoreBoards.Add(scoreboard.ScoreEntry);
+                db.SaveChanges();
+                var scores = from s in db.ScoreBoards
+                             orderby s.Score descending
+                             select s;
+                leaderboard = scores.Take(10).ToList();
+                scoreboard.LeaderBoard = leaderboard;
+                return View(scoreboard); 
+            }
+            scoreboard = (Quiz)Session["quiz"];
+            return View(scoreboard);
+
+        }
+
+
+    
+
+
+	}
+
+
+
+
+}
---
+++ SeQuiz/obj/Release/Package/PackageTmp/Scripts/dragdrop.js
@@ -0,0 +1,181 @@
+
+//buttons
+function start() {
+
+        $.get("Home/StartQuiz", null, function (data) {
+            $('#QuizPane').html(data);
+        });
+
+        document.getElementById("startButton").removeEventListener("click", start, false);
+        document.getElementById("timer").setAttribute("style", "visibility:initial;");
+        theCountDown = setInterval(countingDown, 1000);
+        document.getElementById("status").innerHTML = "Go!";
+
+    
+}
+
+function next() {
+    document.getElementById("nextButton").removeEventListener("click", next, false);
+    var dz1 = document.getElementById("dragzone").firstElementChild;
+    var dz2 = document.getElementById("dragzone2").firstElementChild;
+    var dz3 = document.getElementById("dragzone3").firstElementChild;
+    var dz4 = document.getElementById("dragzone4").firstElementChild;
+    var result;
+    if (dz1 != null && dz2 != null && dz3 != null && dz4 != null) {
+        var position = dz1.getAttribute("src");
+        var position2 = dz2.getAttribute("src");
+        var position3 = dz3.getAttribute("src");
+        var position4 = dz4.getAttribute("src");
+        var num = document.getElementById("CurrentImageset_ImageOrder").getAttribute("value")
+        var order = [num.charAt(0), num.charAt(1), num.charAt(2), num.charAt(3)];
+        if (
+        position.charAt(position.length - 5) == order[0]
+     && position2.charAt(position2.length - 5) == order[1]
+     && position3.charAt(position3.length - 5) == order[2]
+     && position4.charAt(position4.length - 5) == order[3]) {
+
+            document.getElementById("status").innerHTML = "You got it!";
+            result = true;
+            animate(result);
+        }
+        else {
+            document.getElementById("status").innerHTML = "Thats not it...";
+            result = false;
+            animate(result);
+        }
+    }
+    else {
+        document.getElementById("status").innerHTML = "Error";
+    }
+    setTimeout(function () { document.getElementById("status").innerHTML = "Go!"; }, 800);
+}
+
+
+
+function animate(result) {
+    $('#question').addClass('animated slideOutLeft');
+    $('#question').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', setTimeout(function() {advance(result)}, 500));
+}
+function advance(result) {
+    var nxt = document.getElementById("nextButton").textContent;
+    if (nxt == "Next") {
+        $.get("Home/NextQuestion", { match: result, minute: timerminutes, secound: timersecounds }, function (data) {
+            $('#QuizPane').html(data);
+        });
+    }
+    else {
+        clearInterval(theCountDown);
+        window.location.href = "Home/NextQuestion?match=" + result + "&minute=" + timerminutes + "&secound=" + timersecounds;
+    }
+}
+
+
+//timer 
+var timersecounds = 1;
+var timerminutes = 2;
+var theCountDown;
+
+function countingDown() {
+
+    timersecounds--;
+
+
+
+    if (timersecounds < 1 && timerminutes != 0) {
+        timerminutes--;
+        timersecounds = 59;
+    }
+
+    document.getElementById("timer").innerHTML = zeros(timerminutes) + ":" + zeros(timersecounds);
+    if (timerminutes < 1 && timersecounds < 1) {
+        clearInterval(theCountDown);
+        document.getElementById("status").innerHTML =
+        "Times Up";
+        window.location.href = "Home/ScoresOnBoard";
+
+    }
+}
+
+function zeros(t) {
+    if (t < 10) {
+        t = "0" + t;
+    }
+    return t;
+}
+
+//drag, drop and swap
+var draggedfrom;
+function enableDrop(ev) {
+    ev.preventDefault();
+}
+
+function draging(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    draggedfrom = ev.target.parentElement.id;
+}
+
+function droping(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+
+    if (ev.target.nodeName == "DIV") {
+        if (ev.target.firstElementChild == null) {
+            ev.target.appendChild(document.getElementById(data));
+        }
+    }
+    else {
+        ev.target.parentElement.appendChild(document.getElementById(data));
+        document.getElementById(draggedfrom).appendChild(ev.target);
+    }
+}
+
+
+//applying event handlers
+ events();
+ $(document).ajaxComplete(function () {
+    events();
+});
+
+function events() {
+    document.getElementById("startButton").addEventListener("click", start, false);
+    document.getElementById("nextButton").addEventListener("click", next, false);
+
+    var images = document.querySelectorAll(".images");
+    var dragzones = document.querySelectorAll(".dragzones");
+
+
+    //setTimeout(function () {
+        //for (var i = 0; i < images.length; i++) {
+        //    images[i].setAttribute("style", "visibility:initial;");
+        //    $('#' + images[i].id).addClass('animated pulse');
+        //}
+    //}, 1500);
+    document.getElementById("answerpane").addEventListener("load", iloaded, false);
+    $('#answerpane').addClass('animated fadeIn');
+
+
+    //setTimeout(function () {
+    //    for (var i = 0; i < images.length; i++) {
+
+    //        $('#' + images[i].id).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () { $(this).removeClass("animated bounce"); });
+    //    }
+    //}, 1700);
+
+    for (var i = 0; i < images.length; i++) {
+        images[i].addEventListener("dragstart", draging, false);
+
+    }
+
+    for (var i = 0; i < dragzones.length; i++) {
+        dragzones[i].addEventListener("dragover", enableDrop, false);
+        dragzones[i].addEventListener("drop", droping, false);
+    }
+
+
+
+}
+function iloaded() {
+    document.getElementById("answerpane").setAttribute("style", "visibility:initial;");
+    $('#answerpane').addClass('animated fadeIn');
+}
+
